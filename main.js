7ab145144efeb67d8db79940a72f3a41452aa962
Srishti Purohit <srishti.purohit@apsvp.onmicrosoft.com>
2025-10-10 15:57:55 +0530

    Refactored wave drawing functions and organized canvas setup in main.js Completed the Drawing Sine Waves part :)

--- main.js
+++ main.js
@@ -16,10 +16,24 @@ oscillator.type = "sine";
 oscillator.start();
 gainNode.gain.value = 0;
 
-function frequency(pitch) {
-gainNode.gain.setValueAtTime(100, audioCtx.currentTime);
-oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime);
-gainNode.gain.setValueAtTime(0, audioCtx.currentTime + 1);
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var width = ctx.canvas.width;
+var height = ctx.canvas.height;
+var amplitude = 40;
+var interval = null;
+var counter = 0;
+var x = 0;
+var y = height / 2;
+var freq = 0;
+var pitch = 0;
+
+function frequency(notePitch) {
+    pitch = notePitch;
+    gainNode.gain.setValueAtTime(100, audioCtx.currentTime);
+    oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime);
+    gainNode.gain.setValueAtTime(0, audioCtx.currentTime + 1);
+    freq = pitch / 10000;
 }
 
 function handle() {
@@ -28,4 +42,29 @@ function handle() {
     var usernotes = String(input.value);
     var noteFreq = notenames.get(usernotes);
     frequency(noteFreq);
+    drawWave();
+}
+
+function drawWave() {
+    ctx.clearRect(0, 0, width, height);
+    x = 0;
+    y = height / 2;
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    counter = 0;
+    if (interval) { clearInterval(interval); }
+    interval = setInterval(line, 20);
+}
+
+function line() {
+    if (counter > 50) {
+        clearInterval(interval);
+        ctx.stroke();
+        return;
+    }
+    y = height / 2 + (amplitude * Math.sin(x * 2 * Math.PI * freq));
+    ctx.lineTo(x, y);
+    ctx.stroke();
+    x = x + 1;
+    counter++;
 }
