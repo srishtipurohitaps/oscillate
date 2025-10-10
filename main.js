841141ea2cb1f25c9a86c18ad8631339baab84ff
Srishti Purohit <srishti.purohit@apsvp.onmicrosoft.com>
2025-10-10 15:17:04 +0530

    Refactored note frequency handling and initialized notenames map Completed the playing sounds part :)

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ const input = document.getElementById('input');
 const audioCtx = new AudioContext();
 const gainNode = audioCtx.createGain();
 const oscillator = audioCtx.createOscillator();
+notenames = new Map();
+notenames.set("C", 261.6);
+notenames.set("D", 293.7);
+notenames.set("E", 329.6);
+notenames.set("F", 349.2);
+notenames.set("G", 392.0);
+notenames.set("A", 440);
+notenames.set("B", 493.9);
 oscillator.connect(gainNode);
 gainNode.connect(audioCtx.destination);
 oscillator.type = "sine";
@@ -17,5 +25,7 @@ gainNode.gain.setValueAtTime(0, audioCtx.currentTime + 1);
 function handle() {
     audioCtx.resume();
     gainNode.gain.value = 0;
-    frequency(input.value);
+    var usernotes = String(input.value);
+    var noteFreq = notenames.get(usernotes);
+    frequency(noteFreq);
 }
