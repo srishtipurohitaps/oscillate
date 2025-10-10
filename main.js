d739e9fadf38fa45864c22e156ff4b87a2c9b5e3
Srishti Purohit <srishti.purohit@apsvp.onmicrosoft.com>
2025-10-10 13:05:07 +0530

    Fixed some problems in the js code, works fine now :)

--- main.js
+++ main.js
@@ -10,12 +10,12 @@ gainNode.gain.value = 0;
 
 function frequency(pitch) {
 gainNode.gain.setValueAtTime(100, audioCtx.currentTime);
-oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime + 1);
+oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime);
+gainNode.gain.setValueAtTime(0, audioCtx.currentTime + 1);
 }
 
-audioCtx.resume();
-gainNode.gain.value = 0;
 function handle() {
+    audioCtx.resume();
+    gainNode.gain.value = 0;
     frequency(input.value);
-
 }
