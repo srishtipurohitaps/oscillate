22eaa5f2f951cfb899deb55f3fd2f88d3e563571
Srishti Purohit <srishti.purohit@apsvp.onmicrosoft.com>
2025-10-10 12:58:01 +0530

    Added event handler to submit button and initialized audio context

--- main.js
+++ main.js
@@ -5,11 +5,16 @@ const oscillator = audioCtx.createOscillator();
 oscillator.connect(gainNode);
 gainNode.connect(audioCtx.destination);
 oscillator.type = "sine";
+oscillator.start();
+gainNode.gain.value = 0;
 
 function frequency(pitch) {
-
+gainNode.gain.setValueAtTime(100, audioCtx.currentTime);
+oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime + 1);
 }
 
+audioCtx.resume();
+gainNode.gain.value = 0;
 function handle() {
     frequency(input.value);
 
